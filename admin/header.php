82e8faa6b40dcd2da9584b7c9e6e87c35b353e63
mas beyy <contoh@bey.com>
2024-08-09 11:27:54 +0700

    menambahkan logo untuk halaman admin

--- admin/header.php
+++ admin/header.php
@@ -55,9 +55,9 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="../logo_delta.png" style= "width: 40px; height: 200px;" alt="" class="brand-image "
            style="opacity: .8">
-      <span class="brand-text font-weight-light">ADMIN</span>
+      <span class="brand-text font-weight-light">DeltaGrup</span>
     </a>
 
 
